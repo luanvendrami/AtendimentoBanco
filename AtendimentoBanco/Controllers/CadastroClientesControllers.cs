8fd59f8a22f9095c94e5256a09ccf6a2ef312985
luanvendrami <luan.vendrami@hotmail.com>
2021-12-01 16:14:44 -0300

    Criado metodo no controller do cadastro de clientes

--- AtendimentoBanco/Controllers/CadastroClientesControllers.cs
+++ AtendimentoBanco/Controllers/CadastroClientesControllers.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dominio.Interfaces.Aplicações;
+using Dominio.Modelos;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,13 +8,34 @@ using System.Threading.Tasks;
 
 namespace AtendimentoBanco.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
+    [ApiController, Route("[controller]")]
+    [Consumes("application/json")]
     public class CadastroClientesControllers : Controller
     {
+        private readonly ICadastroClienteService _cadastroClienteService;
+
+        public CadastroClientesControllers(ICadastroClienteService cadastroClienteService)
+        {
+            _cadastroClienteService = cadastroClienteService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult CadastroCliente(ClienteDto dto)
+        {
+            try
+            {
+               var retorno = _cadastroClienteService.CadastroCliente(dto);
+                return Ok(retorno);
+            }
+            catch
+            {
+                return BadRequest("As informações NÃO foram salvas!");        
+            }
         }
     }
 }
