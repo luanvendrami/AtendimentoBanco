4274033e5adf56bf16fc6f64f036ce06c06746ae
luanvendrami <luan.vendrami@hotmail.com>
2021-12-01 17:33:47 -0300

    Criado a nova dto para endereco

--- Dominio/Modelos/ClienteDto.cs
+++ Dominio/Modelos/ClienteDto.cs
@@ -8,11 +8,9 @@ namespace Dominio.Modelos
 {
     public class ClienteDto
     {
-        public int Id { get; set; }
         public string NomeCompleto { get; set; }
         public string Cpf { get; set; }
         public string Rg { get; set; }
         public DateTime DataNascimento { get; set; }
-        public string Cep { get; set; }
     }
 }
---
+++ Dominio/Modelos/EnderecoDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Modelos
+{
+    public class EnderecoDto
+    {
+        public int Id { get; set; }
+        public string Uf { get; set; }
+        public string Cidade { get; set; }
+        public string Bairro { get; set; }
+        public string Rua { get; set; }
+        public string NumeroResidencia { get; set; }
+        public string Complemento { get; set; }
+    }
+}
