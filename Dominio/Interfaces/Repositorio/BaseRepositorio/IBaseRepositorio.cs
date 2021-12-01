79e75d921d9d6921f757e3c396f61ca02b18edec
luanvendrami <luan.vendrami@hotmail.com>
2021-12-01 16:15:35 -0300

    Criado o repositorioBase e sua interface

---
+++ Dominio/Interfaces/Repositorio/BaseRepositorio/IBaseRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Interfaces.Repositorio.BaseRepositorio
+{
+    public interface IBaseRepositorio<T> : IDisposable where T : class, new()
+    {
+        void Adicionar(T entity);
+        void Atualizar(T entity);
+        void Remover(int id);
+        int SalvarAlteracao();
+    }
+}
---
+++ Infra.Data/Repositorio/BaseRepositorio/BaseRepositorio.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Infra.Data.Contexto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio.Interfaces.Repositorio.BaseRepositorio;
+
+namespace Infra.Data.Repositorio.BaseRepositorio
+{
+    public abstract class BaseRepositorio<T> : IBaseRepositorio<T> where T : class, new()
+    {
+        protected readonly ContextoSQL _context;
+        private readonly DbSet<T> _dbSet;
+
+        public BaseRepositorio(ContextoSQL context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public void Adicionar(T entity)
+        {
+            _dbSet.Add(entity);
+            SalvarAlteracao();
+        }
+
+        public void Atualizar(T entity)
+        {
+            _dbSet.Update(entity);
+            SalvarAlteracao();
+        }
+
+        public void Dispose()
+        {
+            _context?.Dispose();
+        }
+
+        public void Remover(int id)
+        {
+            _dbSet.Remove(_dbSet.Find(id));
+            SalvarAlteracao();
+        }
+
+        public int SalvarAlteracao()
+        {
+            return _context.SaveChanges();
+        }
+    }
+}
