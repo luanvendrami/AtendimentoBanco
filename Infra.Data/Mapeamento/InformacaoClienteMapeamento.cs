9d0e639754808aaa61449be09db18e931353daa3
luanvendrami <luan.vendrami@hotmail.com>
2021-12-01 16:15:57 -0300

    Criado o repositorio do Cliente e mapeamento;

---
+++ Infra.Data/Mapeamento/InformacaoClienteMapeamento.cs
@@ -0,0 +1,37 @@
+﻿using Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Data.Mapeamento
+{
+    public class InformacaoClienteMapeamento : IEntityTypeConfiguration<InformacaoCliente>
+    {
+        public void Configure(EntityTypeBuilder<InformacaoCliente> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.ToTable("Cliente");
+
+            builder.Property(p => p.NomeCompleto)
+                .IsRequired()
+                .HasColumnType("varchar(70)");
+
+            builder.Property(p => p.Cpf)
+                .IsRequired()
+                .HasColumnType("varchar(11)");
+
+            builder.Property(p => p.Rg)
+                .IsRequired()
+                .HasColumnType("varchar(12)");
+
+            builder.Property(p => p.DataNascimento)
+                .IsRequired()
+                .HasColumnType("datetime");
+        }
+    }
+}
---
+++ Infra.Data/Repositorio/InformacaoClienteRepositorio.cs
@@ -0,0 +1,35 @@
+﻿using Dominio.Entidades;
+using Dominio.Interfaces.Repositorio;
+using Infra.Data.Contexto;
+using Infra.Data.Repositorio.BaseRepositorio;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Data.Repositorio
+{
+    public class InformacaoClienteRepositorio : BaseRepositorio<InformacaoCliente>, IInformacaoClienteRepositorio
+    {
+        public InformacaoClienteRepositorio(ContextoSQL context) : base(context)
+        {
+        }
+
+        public IEnumerable<InformacaoCliente> RetornaClientes()
+        {
+            return _context.InformacaoCliente.AsNoTracking().ToList();
+        }
+
+        public IEnumerable<InformacaoCliente> RetornaNomeCliente(string nome)
+        {
+            return _context.InformacaoCliente.Where(n => n.NomeCompleto.Contains(nome)).ToList();
+        }
+
+        public InformacaoCliente RetornaClientId(int id)
+        {
+            return _context.InformacaoCliente.AsNoTracking().FirstOrDefault(n => n.Id == id);
+        }
+    }
+}
