f17b43edb53654d04ec9644a5065c48a6c49d09c
luanvendrami <luan.vendrami@hotmail.com>
2021-12-01 17:34:01 -0300

    Criado nova library e novo service

---
+++ EnderecoCliente/Service/EnderecoClienteService.cs
@@ -0,0 +1,13 @@
+﻿using Dominio.Interfaces.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnderecoCliente.Service
+{
+    public class EnderecoClienteService : IEnderecoClienteService
+    {
+    }
+}
