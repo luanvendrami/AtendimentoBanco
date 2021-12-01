c3773626fa77a54ab47b5c8cf56de8bf9be9dc0e
luanvendrami <luan.vendrami@hotmail.com>
2021-12-01 16:16:16 -0300

    Adicionado ao metodo de injeção de dependencia o repositorio.

--- Infra.CrossCuting/IoC/ConfigureServicesInjection.cs
+++ Infra.CrossCuting/IoC/ConfigureServicesInjection.cs
@@ -1,6 +1,10 @@
 ﻿using CadastroCliente.Service;
 using Dominio.Interfaces.Aplicações;
+using Dominio.Interfaces.Repositorio;
+using Dominio.Interfaces.Repositorio.BaseRepositorio;
 using Infra.Data.Contexto;
+using Infra.Data.Repositorio;
+using Infra.Data.Repositorio.BaseRepositorio;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Infra.CrossCuting.IoC
@@ -9,12 +13,17 @@ namespace Infra.CrossCuting.IoC
     {
         public static void ConexaoBanco(this IServiceCollection services)
         {
-            services.AddScoped<Contexto>();
+            services.AddScoped<ContextoSQL>();
         }
 
-        public static void InjeçãoServices(this IServiceCollection services)
+        public static void InjeçaoServices(this IServiceCollection services)
         {
             services.AddScoped<ICadastroClienteService, CadastroClienteService>();
         }
+
+        public static void InjeçaoRepositorio(this IServiceCollection services)
+        {
+            services.AddScoped<IInformacaoClienteRepositorio, InformacaoClienteRepositorio>();
+        }   
     }
 }
