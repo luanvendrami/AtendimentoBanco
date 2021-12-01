fb1a8eedc478d5251038d787bae88fd1bb0be4f5
luanvendrami <luan.vendrami@hotmail.com>
2021-12-01 17:32:45 -0300

    Adicionado a nova interface do novo servico

--- Dominio/Interfaces/Service/ICadastroClienteService.cs
+++ Dominio/Interfaces/Service/ICadastroClienteService.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Dominio.Interfaces.Aplicações
+namespace Dominio.Interfaces.Service
 {
     public interface ICadastroClienteService
     {
