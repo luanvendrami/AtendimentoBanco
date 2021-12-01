42800cee7c95cf68130a29e759836321f2173b20
luanvendrami <luan.vendrami@hotmail.com>
2021-12-01 16:16:24 -0300

    Criado a dto.

---
+++ Dominio/Modelos/ClienteDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Modelos
+{
+    public class ClienteDto
+    {
+        public int Id { get; set; }
+        public string NomeCompleto { get; set; }
+        public string Cpf { get; set; }
+        public string Rg { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Cep { get; set; }
+    }
+}
