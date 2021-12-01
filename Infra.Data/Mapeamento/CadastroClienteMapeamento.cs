9cbfef9d23f8b4379a4c4a5ee9c442823ceea16e
luanvendrami <luan.vendrami@hotmail.com>
2021-12-01 17:34:44 -0300

    Alterado mapeamento de clientes e criado da nova tabela de endereco

--- Infra.Data/Mapeamento/CadastroClienteMapeamento.cs
+++ Infra.Data/Mapeamento/CadastroClienteMapeamento.cs
@@ -9,28 +9,24 @@ using System.Threading.Tasks;
 
 namespace Infra.Data.Mapeamento
 {
-    public class InformacaoClienteMapeamento : IEntityTypeConfiguration<InformacaoCliente>
+    public class CadastroClienteMapeamento : IEntityTypeConfiguration<DadosCliente>
     {
-        public void Configure(EntityTypeBuilder<InformacaoCliente> builder)
+        public void Configure(EntityTypeBuilder<DadosCliente> builder)
         {
             builder.HasKey(p => p.Id);
 
             builder.ToTable("Cliente");
 
             builder.Property(p => p.NomeCompleto)
-                .IsRequired()
                 .HasColumnType("varchar(70)");
 
             builder.Property(p => p.Cpf)
-                .IsRequired()
                 .HasColumnType("varchar(11)");
 
             builder.Property(p => p.Rg)
-                .IsRequired()
                 .HasColumnType("varchar(12)");
 
             builder.Property(p => p.DataNascimento)
-                .IsRequired()
                 .HasColumnType("datetime");
         }
     }
---
+++ Infra.Data/Mapeamento/EnderecoClienteMapeamento.cs
@@ -0,0 +1,39 @@
+﻿using Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Data.Mapeamento
+{
+    public class EnderecoClienteMapeamento : IEntityTypeConfiguration<EnderecoCliente>
+    {
+        public void Configure(EntityTypeBuilder<EnderecoCliente> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.ToTable("EnderecoCliente");
+
+            builder.Property(p => p.Uf)
+                .HasColumnType("varchar(2)");
+
+            builder.Property(p => p.Cidade)
+                .HasColumnType("varchar(50)");
+
+            builder.Property(p => p.Bairro)
+                .HasColumnType("varchar(50)");
+
+            builder.Property(p => p.Rua)
+                .HasColumnType("varchar(100)");
+
+            builder.Property(p => p.NumeroResidencia)
+                .HasColumnType("varchar(10)");
+
+            builder.Property(p => p.Complemento)
+                .HasColumnType("varchar(50)");
+        }
+    }
+}
