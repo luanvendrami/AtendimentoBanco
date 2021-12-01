54af4c7c7862fa1bee7c301025bfd8a80301400a
luanvendrami <luan.vendrami@hotmail.com>
2021-12-01 16:14:19 -0300

    Criado service com metodos do CadastroCliente

--- Aplicacao/Service/CadastroClienteService.cs
+++ Aplicacao/Service/CadastroClienteService.cs
@@ -1,4 +1,7 @@
-﻿using Dominio.Interfaces.Aplicações;
+﻿using Dominio.Entidades;
+using Dominio.Interfaces.Aplicações;
+using Dominio.Interfaces.Repositorio;
+using Dominio.Modelos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +12,26 @@ namespace CadastroCliente.Service
 {
     public class CadastroClienteService : ICadastroClienteService
     {
+        private readonly IInformacaoClienteRepositorio _informacaoClienteRepositorio;
+
+        public CadastroClienteService(IInformacaoClienteRepositorio informacaoClienteRepositorio)
+        {
+            _informacaoClienteRepositorio = informacaoClienteRepositorio;
+        }
+
+        public string CadastroCliente(ClienteDto dto)
+        {
+            var entidadeInformacaoCliente = new InformacaoCliente(dto.Id, dto.NomeCompleto, dto.Cpf, dto.Rg, dto.DataNascimento, dto.Cep);
+            if (entidadeInformacaoCliente.Validacao())
+            {
+                _informacaoClienteRepositorio.Adicionar(entidadeInformacaoCliente);
+                _informacaoClienteRepositorio.SalvarAlteracao();
+                return $"Cliente cadastrado com sucesso!";
+            }
+            else
+            {
+                return $"Cliente não foi cadastrado, verifique as informações!";
+            } 
+        }
     }
 }
