6fbf29e07bca27ad90d7503dd03acc5b4434100e
luanvendrami <luan.vendrami@hotmail.com>
2021-12-01 16:15:20 -0300

    INterface do serviço de cliente

--- Dominio/Interfaces/Aplicações/ICadastroClienteService.cs
+++ Dominio/Interfaces/Aplicações/ICadastroClienteService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dominio.Modelos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace Dominio.Interfaces.Aplicações
 {
     public interface ICadastroClienteService
     {
+        string CadastroCliente(ClienteDto dto);
     }
 }
