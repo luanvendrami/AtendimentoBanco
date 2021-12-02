1415d216eee51e901511bc84b8c9cd10078a6519
luanvendrami <luan.vendrami@hotmail.com>
2021-12-02 00:54:18 -0300

    Adicionado ao configure a interface e o repositorio de pagamentos

--- Infra.CrossCuting/IoC/ConfigureServicesInjection.cs
+++ Infra.CrossCuting/IoC/ConfigureServicesInjection.cs
@@ -1,11 +1,9 @@
 ﻿using CadastroCliente.Service;
 using Dominio.Interfaces.Repositorio;
-using Dominio.Interfaces.Repositorio.BaseRepositorio;
 using Dominio.Interfaces.Service;
 using EnderecoCliente.Service;
 using Infra.Data.Contexto;
 using Infra.Data.Repositorio;
-using Infra.Data.Repositorio.BaseRepositorio;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Infra.CrossCuting.IoC
@@ -19,14 +17,16 @@ namespace Infra.CrossCuting.IoC
 
         public static void InjeçaoServices(this IServiceCollection services)
         {
-            services.AddScoped<ICadastroClienteService, CadastroCliente.Service.CadastroClienteService>();
+            services.AddScoped<ICadastroClienteService, CadastroClienteService>();
             services.AddScoped<IEnderecoClienteService, EnderecoClienteService>();
 
         }
 
         public static void InjeçaoRepositorio(this IServiceCollection services)
         {
-            services.AddScoped<ICadastroClienteRepositorio, Data.Repositorio.CadastroClienteRepositorio>();
-        }   
+            services.AddScoped<ICadastroClienteRepositorio, CadastroClienteRepositorio>();
+            services.AddScoped<IEnderecoClienteRepositorio, EnderecoClienteRepositorio>();
+            services.AddScoped<IPagamentosRepositorio, PagamentosRepositorio>();
+        }
     }
 }
