aa48de9a527c530a1485a79911eee92a5e9a3548
luanvendrami <luan.vendrami@hotmail.com>
2021-12-02 15:23:35 -0300

    Criação do service de pagamentos.

---
+++ Pagamentos/Service/PagamentosService.cs
@@ -0,0 +1,36 @@
+﻿using Dominio.Entidades;
+using Dominio.Interfaces.Repositorio;
+using Dominio.Interfaces.Service;
+using Dominio.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pagamentos.Service
+{
+    public class PagamentosService : IPagamentos
+    {
+        private readonly IPagamentosRepositorio _pagamentosRepositorio;
+
+        public PagamentosService(IPagamentosRepositorio pagamentosRepositorio)
+        {
+            _pagamentosRepositorio = pagamentosRepositorio;
+        }
+
+        public string CadastroPagamentos(PagamentoDto dto)
+        {
+            var pagamentosClientesEntidades = new PagamentosCliente(dto.FormaPagamento, dto.ConfirmadoPagamento, dto.ValorPagamentoAgendado, dto.DataPagamentoAgendado, dto.ValorPagamento, dto.DataPagamento, dto.ValorMulta, dto.ValorDesconto);
+            if (pagamentosClientesEntidades.Validacao())
+            {
+                _pagamentosRepositorio.Adicionar(pagamentosClientesEntidades);
+                return $"Pagamento cadastrado com sucesso!";
+            }
+            else
+            {
+                return $"Pagamento não foi cadastrado, verifique as informações!";
+            }
+        }
+    }
+}
