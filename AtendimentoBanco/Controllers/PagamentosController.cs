fadfde762438edd18d0ef33a2fdbdeff611f47af
luanvendrami <luan.vendrami@hotmail.com>
2021-12-02 15:19:11 -0300

    Criação do controller de pagamentos

---
+++ AtendimentoBanco/Controllers/PagamentosController.cs
@@ -0,0 +1,34 @@
+﻿using Dominio.Interfaces.Service;
+using Dominio.Modelos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace AtendimentoBanco.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PagamentosController : ControllerBase
+    {
+        private readonly IPagamentos _pagamentos;
+
+        public PagamentosController(IPagamentos pagamentos)
+        {
+            _pagamentos = pagamentos;
+        }
+
+        [HttpPost]
+        public ActionResult CadastroEndereco([FromForm] PagamentoDto dto)
+        {
+            try
+            {
+                var retorno = _pagamentos.CadastroPagamentos(dto);
+                return Ok(retorno);
+            }
+            catch (Exception ex)
+            {
+                var error = ex;
+                return BadRequest("As informações NÃO foram salvas!");
+            }
+        }
+    }
+}
