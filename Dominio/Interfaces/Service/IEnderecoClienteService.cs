ae350cac28c9c152b1b87ab14eacfbe5f9c65d0d
luanvendrami <luan.vendrami@hotmail.com>
2021-12-02 00:53:53 -0300

    Criado service e interface de endereco

--- Dominio/Interfaces/Service/IEnderecoClienteService.cs
+++ Dominio/Interfaces/Service/IEnderecoClienteService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dominio.Modelos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace Dominio.Interfaces.Service
 {
     public interface IEnderecoClienteService
     {
-
+        string CadastroEndereco(EnderecoDto dto);
     }
 }
--- EnderecoCliente/Service/EnderecoClienteService.cs
+++ EnderecoCliente/Service/EnderecoClienteService.cs
@@ -1,4 +1,7 @@
-﻿using Dominio.Interfaces.Service;
+﻿using Dominio.Interfaces.Repositorio;
+using Dominio.Interfaces.Service;
+using Dominio.Modelos;
+using Dominio.Entidades;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +12,26 @@ namespace EnderecoCliente.Service
 {
     public class EnderecoClienteService : IEnderecoClienteService
     {
+        private readonly IEnderecoClienteRepositorio _enderecoClienteRepositorio;
+
+        public EnderecoClienteService(IEnderecoClienteRepositorio enderecoClienteRepositorio)
+        {
+            _enderecoClienteRepositorio = enderecoClienteRepositorio;
+        }
+
+        public string CadastroEndereco(EnderecoDto dto)
+        {
+            var enderecoClienteEntidade = new EnderecoDoCliente(dto.Uf, dto.Cidade, dto.Bairro, dto.Rua, dto.NumeroResidencia, dto.Complemento);
+            if (enderecoClienteEntidade.Validacao())
+            {
+                _enderecoClienteRepositorio.Adicionar(enderecoClienteEntidade);
+                return $"Endereço cadastrado com sucesso!";
+            }
+            else
+            {
+                return $"Endereço não foi cadastrado, verifique as informações!";
+            }
+
+        }
     }
 }
