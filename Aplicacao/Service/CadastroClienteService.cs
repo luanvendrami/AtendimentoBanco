ba32900ad000cdcf237ae352450b686c80738205
luanvendrami <luan.vendrami@hotmail.com>
2021-12-06 16:55:19 -0300

    Alterados dois metodos o de cadastro e de atualização.

--- Aplicacao/Service/CadastroClienteService.cs
+++ Aplicacao/Service/CadastroClienteService.cs
@@ -2,29 +2,34 @@
 using Dominio.Interfaces.Repositorio;
 using Dominio.Interfaces.Service;
 using Dominio.Modelos;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+       
 
 namespace CadastroCliente.Service
 {
     public class CadastroClienteService : ICadastroClienteService
     {
         private readonly ICadastroClienteRepositorio _informacaoClienteRepositorio;
+        private readonly IEnderecoClienteRepositorio _enderecoClienteMapeamento;
+        private readonly IPagamentosRepositorio _pagamentosRepositorio;
 
-        public CadastroClienteService(ICadastroClienteRepositorio informacaoClienteRepositorio)
+        public CadastroClienteService(ICadastroClienteRepositorio informacaoClienteRepositorio, IEnderecoClienteRepositorio enderecoClienteMapeamento, IPagamentosRepositorio pagamentosRepositorio)
         {
             _informacaoClienteRepositorio = informacaoClienteRepositorio;
+            _enderecoClienteMapeamento = enderecoClienteMapeamento;
+            _pagamentosRepositorio = pagamentosRepositorio;
         }
 
         public string CadastroCliente(ClienteDto dto)
         {
-            var entidadeInformacaoCliente = new DadosCliente(dto.NomeCompleto, dto.Cpf, dto.Rg, dto.DataNascimento);
-            if (entidadeInformacaoCliente.Validacao())
+            var entidadeCliente = new DadosCliente(dto.NomeCompleto, dto.Cpf, dto.Rg, dto.DataNascimento);
+            var entidadeEndereco = new EnderecoDoCliente(entidadeCliente, dto.Uf, dto.Cidade, dto.Bairro, dto.Rua, dto.NumeroResidencia, dto.Complemento);
+            var entidadePagamento = new PagamentosCliente(entidadeCliente, dto.FormaPagamento, dto.ConfirmadoPagamento, dto.ValorPagamentoAgendado, dto.DataPagamentoAgendado, dto.ValorPagamento, dto.DataPagamento, dto.ValorMulta, dto.ValorDesconto);
+            if (entidadeCliente.Validacao() && entidadeEndereco.Validacao() && entidadePagamento.Validacao())
             {
-                _informacaoClienteRepositorio.Adicionar(entidadeInformacaoCliente);
+                _informacaoClienteRepositorio.Adicionar(entidadeCliente);
+                _enderecoClienteMapeamento.Adicionar(entidadeEndereco);
+                _pagamentosRepositorio.Adicionar(entidadePagamento);
+                
                 return $"Cliente cadastrado com sucesso!";
             }
             else
@@ -35,7 +40,7 @@ namespace CadastroCliente.Service
 
         public DadosCliente RetornaPorId(int id)
         {
-            var consultaRepositorio = _informacaoClienteRepositorio.RetornaClientId(id);
+            var consultaRepositorio = _informacaoClienteRepositorio.ConsultaId(id);
             return consultaRepositorio;
         }
 
@@ -46,7 +51,14 @@ namespace CadastroCliente.Service
             {    
                 var dadosCliente = new DadosCliente(consultaRepositorio.Id, dto.NomeCompleto, dto.Cpf, dto.Rg, dto.DataNascimento);
                 consultaRepositorio = dadosCliente;
+                var entidadeEndereco = new EnderecoDoCliente(consultaRepositorio.Id, dto.Uf, dto.Cidade, dto.Bairro, dto.Rua, dto.NumeroResidencia, dto.Complemento);
+                consultaRepositorio.Endereco = entidadeEndereco;
+                var entidadePagamento = new PagamentosCliente(consultaRepositorio.Id, dto.FormaPagamento, dto.ConfirmadoPagamento, dto.ValorPagamentoAgendado, dto.DataPagamentoAgendado, dto.ValorPagamento, dto.DataPagamento, dto.ValorMulta, dto.ValorDesconto);
+                consultaRepositorio.Pagamentos = entidadePagamento;
+
                 _informacaoClienteRepositorio.Atualizar(consultaRepositorio);
+                _enderecoClienteMapeamento.Atualizar(entidadeEndereco);
+                _pagamentosRepositorio.Atualizar(entidadePagamento);
             }
             return $"Alterado com sucesso!";
         }
