f108ac018211f8b875ce56ca4575c014642bc89c
luanvendrami <luan.vendrami@hotmail.com>
2021-12-06 16:56:32 -0300

    Excluido as Dto's e juntados apenas em uma.

--- Dominio/Modelos/ClienteDto.cs
+++ Dominio/Modelos/ClienteDto.cs
@@ -12,5 +12,21 @@ namespace Dominio.Modelos
         public string Cpf { get; set; }
         public string Rg { get; set; }
         public DateTime DataNascimento { get; set; }
+
+        public string Uf { get; set; }
+        public string Cidade { get; set; }
+        public string Bairro { get; set; }
+        public string Rua { get; set; }
+        public string NumeroResidencia { get; set; }
+        public string Complemento { get; set; }
+
+        public int FormaPagamento { get; set; }
+        public bool ConfirmadoPagamento { get; set; }
+        public decimal ValorPagamentoAgendado { get; set; }
+        public DateTime DataPagamentoAgendado { get; set; }
+        public decimal ValorPagamento { get; set; }
+        public DateTime DataPagamento { get; set; }
+        public decimal ValorMulta { get; set; }
+        public decimal ValorDesconto { get; set; }
     }
 }
