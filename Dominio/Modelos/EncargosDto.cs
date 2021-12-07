0b0d9704913b9eeaceef5017ea25b23de8e9791a
luanvendrami <luan.vendrami@hotmail.com>
2021-12-07 18:01:03 -0300

    Criado novo dto para encargos

---
+++ Dominio/Modelos/EncargosDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Modelos
+{
+    public class EncargosDto
+    {
+        public decimal Juros { get; private set; }
+        public decimal Desconto { get; private set; }
+    }
+}
