b7eea79991d1e6ad2f2167e1f3198cbb35cb02db
luanvendrami <luan.vendrami@hotmail.com>
2021-12-07 17:43:23 -0300

    Adicionado as propriedades na entidade de PagamentoTaxas

---
+++ Dominio/Entidades/PagamentoTaxas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Entidades
+{
+    public class PagamentoTaxas
+    {
+        public int Id { get; private set; }
+        public decimal Juros { get; private set; }
+        public decimal Desconto { get; private set; }
+        public int IdPagamento { get; private set; }
+
+        public PagamentoTaxas(int id, decimal juros, decimal desconto, int idPagamento)
+        {
+            Id = id;
+            Juros = juros;
+            Desconto = desconto;
+            IdPagamento = idPagamento;
+        }
+
+        public PagamentoTaxas()
+        {
+
+        }
+    }
+}
