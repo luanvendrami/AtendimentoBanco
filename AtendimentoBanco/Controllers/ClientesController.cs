007d3bffacfcb762b240598e91246ffd8b20f707
luanvendrami <luan.vendrami@hotmail.com>
2021-12-07 18:03:30 -0300

    Alterado o nome da dto antiga

--- AtendimentoBanco/Controllers/ClientesController.cs
+++ AtendimentoBanco/Controllers/ClientesController.cs
@@ -48,7 +48,7 @@ namespace AtendimentoBanco.Controllers
         }
 
         [HttpPost("CadastroCliente")]
-        public ActionResult CadastroCliente([FromForm]ClienteDto dto)
+        public ActionResult CadastroCliente([FromForm] DadosDto dto)
         {
             try
             {
@@ -62,7 +62,7 @@ namespace AtendimentoBanco.Controllers
         }
 
         [HttpPut("AtualizarCadastroCliente")]
-        public ActionResult AtualizarCliente(int id, [FromForm] ClienteDto dto)
+        public ActionResult AtualizarCliente(int id, [FromForm] DadosDto dto)
         {
             try
             {
