1bb1e64b08692662df50ea56b58b74afff951226
luanvendrami <luan.vendrami@hotmail.com>
2021-12-07 17:30:17 -0300

    Criado a interface e o serviço para o Pagamento

---
+++ Pagamento/Service/PagametnoService.cs
@@ -0,0 +1,15 @@
+﻿using Dominio.Interfaces.Service;
+using Dominio.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pagamento.Service
+{
+    public class PagametnoService : IPagamentoService
+    {
+
+    }
+}
