45a14ed9e8f330e1147341a6905d5ab93bd49dff
luanvendrami <luan.vendrami@hotmail.com>
2021-12-07 17:44:09 -0300

    Mapeamento da nova tabela de PagamentoTaxas

---
+++ Infra.Data/Mapeamento/PagamentoTaxasMapeamento.cs
@@ -0,0 +1,33 @@
+﻿using Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Data.Mapeamento
+{
+    public class PagamentoTaxasMapeamento : IEntityTypeConfiguration<PagamentoTaxas>
+    {
+        public void Configure(EntityTypeBuilder<PagamentoTaxas> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.ToTable("PagamentoTaxas");
+
+            builder.Property(p => p.Juros)
+                .HasColumnType("decimal")
+                .IsRequired();
+
+            builder.Property(p => p.Desconto)
+                .HasColumnType("decimal")
+                .IsRequired();
+
+            builder.Property(p => p.IdPagamento)
+                .HasColumnType("int")
+                .IsRequired();
+        }
+    }
+}
