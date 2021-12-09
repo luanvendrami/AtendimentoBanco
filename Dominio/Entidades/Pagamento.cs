f02171101f7f683cb97c9f8f8fd484ca25fef052
luanvendrami <luan.vendrami@hotmail.com>
2021-12-08 22:34:36 -0300

    Alterado entidades para poderem ser nullas

--- Dominio/Entidades/Pagamento.cs
+++ Dominio/Entidades/Pagamento.cs
@@ -12,43 +12,36 @@ namespace Dominio.Entidades
     {  
         public int Id { get; private set; }
         public int FormaPagamento { get; private set; }
-        public bool ConfirmadoPagamento { get; private set; }
-        public decimal ValorPago { get; private set; }
-        public DateTime DataVencimento { get; private set; }
-        public decimal ValorPagamento { get; private set; }
-        public DateTime DataPagamento { get; private set; }
-        public decimal Juros { get; private set; }
-        public decimal Desconto { get; private set; }
         public int IdCliente { get; private set; }
+
+#nullable enable
+        public decimal? ValorPago { get; private set; }
+
+        public DateTime? DataVencimento { get; private set; }
+
+        //DATA DE COBRANÇA
+        public DateTime? DataPagamento { get; private set; }
+        public decimal? Juros { get; private set; }
+        public decimal? Desconto { get; private set; }
+        public decimal? ValorPagamentoComReajustes { get; private set; }
+#nullable disable
 
         [ForeignKey("IdCliente")]
         public virtual Cliente PagamentoNavegation { get; set; }
 
         //Construtor usada para o metodo de cadastro de clientes.
-        public Pagamento(Cliente navegation, TipoPagamento formaPagamento, bool confirmadoPagamento, decimal valorPago, DateTime vencimento, DateTime dataPagamento, decimal juros, decimal desconto, decimal valorPagamento)
+        public Pagamento(Cliente navegation, TipoPagamento formaPagamento)
         {
             PagamentoNavegation = navegation;
-            FormaPagamento = (int)formaPagamento;
-            ConfirmadoPagamento = confirmadoPagamento;
-            ValorPago = valorPago;
-            DataVencimento = vencimento;           
-            DataPagamento = dataPagamento;    
-            Juros = PagamentoAtrasado(formaPagamento, dataPagamento, vencimento, valorPago, juros);
-            Desconto = PagamentoAdiantado(formaPagamento, dataPagamento, vencimento, valorPago, desconto);
-            ValorPagamento = ValoresFinais(valorPagamento, Juros, Desconto);      
+            FormaPagamento = (int)formaPagamento;  
             Validacao();
         }
 
         //Construtor usada para o metodo atualização de cadastro para clientes.
-        public Pagamento(int idCliente, TipoPagamento formaPagamento, bool confirmadoPagamento, decimal valorPagamentoAgendado, DateTime vencimento, decimal valorPago, DateTime dataPagamento)
+        public Pagamento(int idCliente, TipoPagamento formaPagamento)
         {
             IdCliente = idCliente;
             FormaPagamento = (int)formaPagamento;
-            ConfirmadoPagamento = confirmadoPagamento;
-            ValorPago = valorPagamentoAgendado;
-            DataVencimento = vencimento;
-            ValorPagamento = valorPago; 
-            DataPagamento = dataPagamento;
             Validacao();
         }
 
@@ -59,7 +52,7 @@ namespace Dominio.Entidades
 
         public bool Validacao()
         {
-            if (!string.IsNullOrEmpty(FormaPagamento.ToString()) && !string.IsNullOrEmpty(ConfirmadoPagamento.ToString()) && !string.IsNullOrEmpty(ValorPago.ToString()) && !string.IsNullOrEmpty(ValorPagamento.ToString()) && !string.IsNullOrEmpty(DataPagamento.ToString()))
+            if (!string.IsNullOrEmpty(FormaPagamento.ToString()))
                 return true;
             return false;
         }
@@ -154,18 +147,23 @@ namespace Dominio.Entidades
             return desconto;
         }
 
-        public static decimal ValoresFinais(decimal valorPagamento, decimal juros, decimal desconto)
+        public static decimal ValoresFinais(decimal valorPagamentoComReajustes, decimal juros, decimal desconto)
         {
             if(juros != 0)
             {
-                valorPagamento += juros;
-                return valorPagamento;
+                valorPagamentoComReajustes += juros;
+                return valorPagamentoComReajustes;
             }else if(desconto != 0)
             {
-                valorPagamento -= desconto;
-                return valorPagamento;
+                valorPagamentoComReajustes -= desconto;
+                return valorPagamentoComReajustes;
             }
-            return valorPagamento;
+            return valorPagamentoComReajustes;
         }
+
+        //Juros = PagamentoAtrasado(formaPagamento, dataPagamento, DataVencimento, valorPago, juros);
+        //Desconto = PagamentoAdiantado(formaPagamento, dataPagamento, DataVencimento, valorPago, desconto);
+        //ValorPagamentoComReajustes = ValoresFinais(valorPagamentoComReajustes, Juros, Desconto);
+        //DataVencimento = DateTime.Now.AddDays(30);  
     }
 }
