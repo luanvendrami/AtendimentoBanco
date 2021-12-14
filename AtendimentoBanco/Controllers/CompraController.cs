fa8a40e6c2ac08a2c9a4c97681dee42495c6d0d1
luanvendrami <luan.vendrami@hotmail.com>
2021-12-14 00:03:02 -0300

    Criado o controller para a compra

---
+++ AtendimentoBanco/Controllers/CompraController.cs
@@ -0,0 +1,21 @@
+﻿using Dominio.Interfaces.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AtendimentoBanco.Controllers
+{
+    [ApiController, Route("[controller]")]
+    public class CompraController : ControllerBase
+    {
+        private readonly ICompraService _compraService;
+
+        public CompraController(ICompraService compraService)
+        {
+            _compraService = compraService;
+        }
+    }
+}
