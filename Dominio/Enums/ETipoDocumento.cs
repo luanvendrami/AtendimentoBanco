3f4ce1a032088efc3fede3c22e0f4d7da8f096ab
Luan Vendrami <luan.vendrami@havan.com.br>
2021-12-16 17:53:44 -0300

    Enum para ser usado nas entidades

---
+++ Dominio/Enums/ETipoDocumento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Enums
+{
+    public enum ETipoDocumento
+    {
+        CPF = 1,
+        CNPJ = 2
+    }
+}
