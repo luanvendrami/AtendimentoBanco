8a9deba9f8eb64dfed02b0a605641a232e2a94c4
Luan Vendrami <luan.vendrami@havan.com.br>
2021-12-17 18:02:14 -0300

    Criado teste para as validações do email

---
+++ Testes/EmailTEste.cs
@@ -0,0 +1,28 @@
+﻿using Dominio.Entidades.ValueObject;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testes
+{
+    [TestClass]
+    public class EmailTEste
+    {
+        [TestMethod]
+        public void ValidaEmailFail()
+        {
+            var mail = new Email("luan.vendrami");
+            Assert.IsFalse(mail.ValidaEmail(mail.Adress));
+        }
+
+        [TestMethod]
+        public void ValidaEmailValid()
+        {
+            var mail = new Email("luan.vendrami@hotmail.com");
+            Assert.IsTrue(mail.ValidaEmail(mail.Adress));
+        }
+    }
+}
