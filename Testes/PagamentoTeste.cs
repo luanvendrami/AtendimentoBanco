29aa94bffee2d158dd1584dcf6bd7ab2509911b3
luanvendrami <luan.vendrami@hotmail.com>
2021-12-18 01:57:45 -0300

    Adicionado teste para pagamentos

---
+++ Testes/PagamentoTeste.cs
@@ -0,0 +1,22 @@
+﻿using Dominio.Entidades;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testes
+{
+    [TestClass]
+    public class PagamentoTeste
+    {
+
+        [TestMethod]
+        public void ValidaPagamentoFail()
+        {
+            var doc = new Pagamento("123", ETipoDocumento.CPF);
+            Assert.IsNotNull(doc.Validacao());
+        }
+    }
+}
