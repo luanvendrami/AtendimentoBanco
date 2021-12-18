3c9425cb8a68cd6897bab5a84eeda6fc3a566c26
luanvendrami <luan.vendrami@hotmail.com>
2021-12-18 01:57:29 -0300

    Alterado nameSpace das classes

---
+++ Testes/ValueObject/NameTeste.cs
@@ -0,0 +1,28 @@
+﻿using Dominio.Entidades.ValueObject;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testes.ValueObject
+{
+    [TestClass]
+    public class NameTeste
+    {
+        [TestMethod]
+        public void ValidaNomeFail()
+        {
+            var end = new Nome("", "say");
+            Assert.IsFalse(end.ValidaNulo());
+        }
+
+        [TestMethod]
+        public void ValidaNomeValid()
+        {
+            var end = new Nome("Nome", "sobrenome");
+            Assert.IsTrue(end.ValidaNulo());
+        }
+    }
+}
