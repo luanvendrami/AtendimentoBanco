b4e5b8b10d582f0e1519f149eff28c83a196fda9
luanvendrami <luan.vendrami@hotmail.com>
2021-12-18 01:51:49 -0300

    Criado classe de testes para o telefone

---
+++ Testes/TelefoneTeste.cs
@@ -0,0 +1,43 @@
+﻿using Dominio.Entidades.ValueObject;
+using Dominio.Enums;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testes
+{
+    [TestClass]
+    public class TelefoneTeste
+    {
+        [TestMethod]
+        public void ValidaTelefoneCelularFail()
+        {
+            var tel = new Telefone("996605059", ETipoTelefone.CELULAR);
+            Assert.IsFalse(tel.ValidaTelefone());
+        }
+
+        [TestMethod]
+        public void ValidaTelefoneCelularValid()
+        {
+            var tel = new Telefone("47996605059", ETipoTelefone.CELULAR);
+            Assert.IsTrue(tel.ValidaTelefone());
+        }
+
+        [TestMethod]
+        public void ValidaTelefoneResidencialFail()
+        {
+            var tel = new Telefone("33841156", ETipoTelefone.RESIDENCIAL);
+            Assert.IsFalse(tel.ValidaTelefone());
+        }
+
+        [TestMethod]
+        public void ValidaTelefoneResidencialValid()
+        {
+            var tel = new Telefone("4733841156", ETipoTelefone.RESIDENCIAL);
+            Assert.IsTrue(tel.ValidaTelefone());
+        }
+    }
+}
