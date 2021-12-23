8c44435fc88bda223005eb4da51762b8efda2b06
luanvendrami <luan.vendrami@hotmail.com>
2021-12-23 18:06:41 -0300

    Adicionado validador para os parametros na entidade

--- Dominio/Entidades/Pagamento.cs
+++ Dominio/Entidades/Pagamento.cs
@@ -7,8 +7,10 @@ using System.Threading.Tasks;
 
 namespace Dominio.Entidades
 {
-    public abstract class Pagamento
+    public abstract class Pagamento 
     {
+        private List<string> _notificacao;
+
         public string Numero { get; private set; }
         public DateTime DataPagemento { get; private set; }
         public DateTime DataVencimento { get; private set; }
@@ -31,5 +33,51 @@ namespace Dominio.Entidades
             EnderecoCobranca = enderecoCobranca;
             Email = email;
         } 
+
+        public string Validar()
+        {
+            if (string.IsNullOrEmpty(DataVencimento.ToString()))
+            {
+                _notificacao.Add("Erro na data de vencimento.");
+                throw new ArgumentNullException(nameof(DataVencimento));
+            }
+
+            if (string.IsNullOrEmpty(Total.ToString()))
+            {
+                _notificacao.Add("Erro no total inserido.");
+                throw new ArgumentNullException(nameof(Total));
+            }
+
+            if (string.IsNullOrEmpty(TotalPago.ToString()))
+            {
+                _notificacao.Add("Erro no total a pagar inserido.");
+                throw new ArgumentNullException(nameof(TotalPago));
+            }
+
+            if (string.IsNullOrEmpty(ProprietarioPagamento.ToString()))
+            {
+                _notificacao.Add("Erro ao inserir o Prorprietario do pagamento.");
+                throw new ArgumentNullException(nameof(ProprietarioPagamento));
+            }
+
+            if (string.IsNullOrEmpty(Documento.ToString()))
+            {
+                _notificacao.Add("Erro ao inserir o documento.");
+                throw new ArgumentNullException(nameof(Documento));
+            }
+
+            if (string.IsNullOrEmpty(EnderecoCobranca.ToString()))
+            {
+                _notificacao.Add("Erro ao inserir o erro de cobrança");
+                throw new ArgumentNullException(nameof(EnderecoCobranca));
+            }
+
+            if (string.IsNullOrEmpty(Email.ToString()))
+            {
+                _notificacao.Add("Erro ao inserir o e-mail.");
+                throw new ArgumentNullException(nameof(Email));
+            }
+            return "Todos os campos válidos.";
+        }
     }
 }
